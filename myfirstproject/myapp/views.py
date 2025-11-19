f27a4928610505b6cb99b6f267da3ec75bce8e8c
Daniel <danilrodionov8665@gmail.com>
2025-11-19 22:09:01 +0300

    practika 13

---
+++ myfirstproject/myapp/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+	return render(request, "index.html")
+
+
+def about(request):
+	return render(request, "about.html")
+
+
+def contact(request):
+	return render(request, "contact.html")
+
+
+def lesson2(request):
+	return render(request, "lesson2.html")
+
+
+
